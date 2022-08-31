1605cf7b0175e832b362831dd0338e2a36f77876
pomazkovanton <vitrumbeta@gmail.com>
2022-08-31 14:12:55 +0800

    fix: add lost code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import AppHeader from './components/AppHeader/AppHeader';
+import BurgerConstructor from './components/BurgerConstructor/BurgerConstructor';
+import BurgerIngredients from './components/BurgerIngredients/BurgerIngredients';
+import { IngredientType } from './types/Ingredient';
+
+import data from './utils/data.json';
 
 const App: React.FC = () => {
-  return <AppHeader />;
+  const [ingredients, setIngredients] = useState(data);
+  const [burger, setBurger] = useState<IngredientType[]>([]);
+
+  const addToBurger = (ingredient: IngredientType) => {
+    burger.includes(ingredient)
+      ? setBurger(burger.filter((el) => el !== ingredient))
+      : setBurger([...burger, ingredient]);
+  };
+
+  return (
+    <>
+      <AppHeader />
+      <main className='container'>
+        <h1 className='text text_type_main-large mt-10'>Соберите бургер</h1>
+        <div className='wrapper'>
+          <BurgerIngredients ingredients={ingredients} addToBurger={addToBurger} />
+          <BurgerConstructor burger={burger} />
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default App;
