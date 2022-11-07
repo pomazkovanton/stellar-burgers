2deb8d9f4384101ae5ae53a3266ede7bd59a947b
pomazkovanton <vitrumbeta@gmail.com>
2022-11-07 16:06:12 +0800

    feat(api): add a header to the server request handler

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 //Универсальный обработчик запроса на сервер
-export const handleRequest = async (url: string, method: string, data = {}) => {
+export const handleRequest = async (url: string, method: string, data = {}, headers = {}) => {
   const res = await axios(url, {
     method: method,
     data: data,
+    headers: headers,
   });
   return res;
 };
