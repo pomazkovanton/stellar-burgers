00b31498405ee59b4b296d7a1767889025a5f765
pomazkovanton <vitrumbeta@gmail.com>
2022-11-22 10:52:28 +0800

    refactor: move the order card to a separate component

---
+++ src/components/CardOrder/CardOrder.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+
+import styles from './cardorder.module.css';
+
+const CardOrder = ({ order, ingredients }) => {
+  const price = order.ingredients
+    .map((id) => ingredients.find((ingr) => ingr._id === id))
+    .reduce((acc, ingr) => acc + ingr.price, 0);
+
+  return (
+    <article className={styles.card}>
+      <div className={styles.header}>
+        <p className='text text_type_digits-default'>#{order.number}</p>
+        <data className='text text_type_main-default text_color_inactive'>{order.createdAt}</data>
+      </div>
+      <h3 className='text text_type_main-medium'>{order.name}</h3>
+      <div className={styles.main}>
+        <ul className={styles.ingredientsList}>
+          {order.ingredients.map((id, index) => {
+            if (index <= 5) {
+              const { image } = ingredients.filter((ingr) => ingr._id === id)[0];
+              return (
+                <li
+                  key={uuidv4()}
+                  style={{
+                    transform: `translateX(${0 - 16 * index}px)`,
+                    zIndex: `${5 - index}`,
+                  }}
+                  className={styles.ingredientsItem}
+                >
+                  <img className={styles.ingredientsImg} src={image} alt='' />
+                  {index === 5 && (
+                    <div className={styles.counter}>
+                      <span className={`text text_type_digits-default ${styles.counterText}`}>
+                        {`+${order.ingredients.length - 5}`}
+                      </span>
+                    </div>
+                  )}
+                </li>
+              );
+            }
+          })}
+        </ul>
+        <p className={styles.price}>
+          <span className='text text_type_digits-default'>{price}</span>
+          <CurrencyIcon type='primary' />
+        </p>
+      </div>
+    </article>
+  );
+};
+
+export default CardOrder;
--- src/pages/FeedPage/FeedPage.tsx
+++ src/pages/FeedPage/FeedPage.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid';
+import CardOrder from 'src/components/CardOrder/CardOrder';
 
-import { CurrencyIcon, FormattedDate } from '@ya.praktikum/react-developer-burger-ui-components';
 import StatisticsBoard from '../../components/StatisticsBoard/StatisticsBoard';
 
 import { connect, disconnect } from '../../store/slices/wsSlice';
@@ -12,6 +11,7 @@ import styles from './feedpage.module.css';
 const FeedPage = () => {
   const { data, isConnected } = useSelector((state) => state.ws);
   const { ingredients } = useSelector((state) => state.ingredients);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -31,56 +31,9 @@ const FeedPage = () => {
           <div className={styles.mainWrapper}>
             <ul className={styles.orderList}>
               {data.orders.map((order) => {
-                const price = order.ingredients
-                  .map((id) => ingredients.find((ingr) => ingr._id === id))
-                  .reduce((acc, ingr) => acc + ingr.price, 0);
-
                 return (
                   <li key={order._id}>
-                    <article className={styles.card}>
-                      <div className={styles.header}>
-                        <p className='text text_type_digits-default'>#{order.number}</p>
-                        <data className='text text_type_main-default text_color_inactive'>
-                          {order.createdAt}
-                          {/* Сегодня, 16:20 i-GMT+3 */}
-                        </data>
-                      </div>
-                      <h3 className='text text_type_main-medium'>{order.name}</h3>
-                      <div className={styles.main}>
-                        <ul className={styles.ingredientsList}>
-                          {order.ingredients.map((id, index) => {
-                            if (index <= 5) {
-                              const { image } = ingredients.filter((ingr) => ingr._id === id)[0];
-                              return (
-                                <li
-                                  key={uuidv4()}
-                                  style={{
-                                    transform: `translateX(${0 - 16 * index}px)`,
-                                    zIndex: `${5 - index}`,
-                                  }}
-                                  className={styles.ingredientsItem}
-                                >
-                                  <img className={styles.ingredientsImg} src={image} alt='' />
-                                  {index === 5 && (
-                                    <div className={styles.counter}>
-                                      <span
-                                        className={`text text_type_digits-default ${styles.counterText}`}
-                                      >
-                                        {`+${order.ingredients.length - 5}`}
-                                      </span>
-                                    </div>
-                                  )}
-                                </li>
-                              );
-                            }
-                          })}
-                        </ul>
-                        <p className={styles.price}>
-                          <span className='text text_type_digits-default'>{price}</span>
-                          <CurrencyIcon type='primary' />
-                        </p>
-                      </div>
-                    </article>
+                    <CardOrder order={order} ingredients={ingredients} />
                   </li>
                 );
               })}
