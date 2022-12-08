eed13c179d0701f277125873b91262a16a8b5505
pomazkovanton <vitrumbeta@gmail.com>
2022-12-08 14:14:47 +0800

    refactor(ts): type ingredient page

--- src/pages/IngredientPage/IngredientPage.tsx
+++ src/pages/IngredientPage/IngredientPage.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 import IngredientDetails from '../../components/BurgerIngredients/IngredientDetails/IngredientDetails';
+import Loader from '../../components/Loader/Loader';
+
 import { lOADING_DATA, REJECTED_DATA, RESOLVED_DATA } from '../../utils/constans';
+import { useAppSelector } from '../../utils/hooks';
 
 import styles from './ingredientpage.module.css';
 
-const IngredientPage = () => {
-  const { id } = useParams();
-  const { ingredients, ingredientsStatus, ingredientsError } = useSelector(
+type TParams = {
+  id: string;
+};
+
+const IngredientPage: React.FC = () => {
+  const { id } = useParams<TParams>();
+  const { ingredients, ingredientsStatus, ingredientsError } = useAppSelector(
     (store) => store.ingredients,
   );
   const ingredient = ingredients.find((item) => item._id === id);
 
   return (
     <div className={styles.container}>
-      {ingredientsStatus === lOADING_DATA && (
-        <h2 className='text text_type_main-large'>Загрузка данных...</h2>
-      )}
+      {ingredientsStatus === lOADING_DATA && <Loader />}
       {ingredientsStatus === REJECTED_DATA && (
         <h2 className='text text_type_main-large'>Ошибка загрузки данных: {ingredientsError}</h2>
       )}
