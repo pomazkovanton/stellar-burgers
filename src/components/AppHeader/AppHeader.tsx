e913cc27ff5aea65081c4fddc6d93404a8e34efb
pomazkovanton <vitrumbeta@gmail.com>
2022-12-14 10:34:14 +0800

    feat(header): add a user name to the site header when logging in

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -6,10 +6,12 @@ import NavItem from './NavItem/NavItem';
 import MainNav from './MainNav/MainNav';
 
 import { HOME_ROUTE, PROFILE_ROUTE } from '../../utils/constans';
+import { useAppSelector } from '../../utils/hooks';
 
 import styles from './appheader.module.css';
 
 const AppHeader: React.FC = () => {
+  const { isAuth, user } = useAppSelector((state) => state.auth);
   return (
     <header className={`pt-3 pb-3 ${styles.header}`}>
       <div className={styles.content}>
@@ -27,7 +29,9 @@ const AppHeader: React.FC = () => {
         <ul className={styles.list}>
           <NavItem url={PROFILE_ROUTE}>
             <ProfileIcon type='secondary' />
-            <p className='text text_type_main-default text_color_inactive ml-2'>Личный кабинет</p>
+            <p className='text text_type_main-default text_color_inactive ml-2'>
+              {isAuth && user ? user.name : 'Личный кабинет'}
+            </p>
           </NavItem>
         </ul>
       </div>
