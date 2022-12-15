72f0b1190b1eaf409d47b1fb3a622a792e1e4e5e
pomazkovanton <vitrumbeta@gmail.com>
2022-12-15 14:29:50 +0800

    fix(dnd): fix problems with dnd in Firefox

--- src/components/BurgerIngredients/Ingredient/Ingredient.tsx
+++ src/components/BurgerIngredients/Ingredient/Ingredient.tsx
@@ -31,26 +31,22 @@ const Ingredient: React.FC<IngredientProps> = ({ ingredient }) => {
   };
 
   return (
-    <Link
-      className={styles.link}
-      to={{ pathname: `${INGREDIENTS_ROUTE}/${ingredient._id}`, state: { background: location } }}
-    >
-      <div
-        onMouseUp={handleClick}
-        className={styles.card}
-        role='presentation'
-        draggable={true}
-        ref={dragRef}
+    <div ref={dragRef}>
+      <Link
+        className={styles.link}
+        to={{ pathname: `${INGREDIENTS_ROUTE}/${ingredient._id}`, state: { background: location } }}
       >
-        <img className={styles.image} src={ingredient.image} alt={ingredient.name} />
-        <div className={styles.price}>
-          <span className='text text_type_digits-default'>{ingredient.price}</span>
-          <CurrencyIcon type='primary' />
+        <div onMouseUp={handleClick} className={styles.card} role='presentation' draggable={true}>
+          <img className={styles.image} src={ingredient.image} alt={ingredient.name} />
+          <div className={styles.price}>
+            <span className='text text_type_digits-default'>{ingredient.price}</span>
+            <CurrencyIcon type='primary' />
+          </div>
+          <p className={`text text_type_main-default ${styles.name}`}>{ingredient.name}</p>
+          {count !== 0 ? <Counter count={count} size='default' /> : null}
         </div>
-        <p className={`text text_type_main-default ${styles.name}`}>{ingredient.name}</p>
-        {count !== 0 ? <Counter count={count} size='default' /> : null}
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 };
 
